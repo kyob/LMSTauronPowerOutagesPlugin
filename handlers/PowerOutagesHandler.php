5a0859cb70ff2cf59126ea2e1f4f68cb904024e1
Łukasz Kopiszka <lukasz@alfa-system.pl>
2020-06-03 10:47:39 +0200

    first code realese

---
+++ handlers/PowerOutagesHandler.php
@@ -0,0 +1,66 @@
+<?php
+
+class PowerOutagesHandler
+{
+    public function menuPowerOutages(array $hook_data = array())
+    {
+        $po_submenus = array(
+            array(
+                'name' => trans('Power outages'),
+                'link' => '?m=poweroutages',
+                'tip' => trans('Power outages'),
+                'prio' => 150,
+            ),
+        );
+        $hook_data['admin']['submenu'] = array_merge($hook_data['admin']['submenu'], $po_submenus);
+        return $hook_data;
+    }
+
+    public function smartyPowerOutages(Smarty $hook_data)
+    {
+        $template_dirs = $hook_data->getTemplateDir();
+        $plugin_templates = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSPowerOutagesPlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'templates';
+        array_unshift($template_dirs, $plugin_templates);
+        $hook_data->setTemplateDir($template_dirs);
+        return $hook_data;
+    }
+
+    public function modulesDirPowerOutages(array $hook_data = array())
+    {
+        $plugin_modules = PLUGINS_DIR . DIRECTORY_SEPARATOR . LMSPowerOutagesPlugin::PLUGIN_DIRECTORY_NAME . DIRECTORY_SEPARATOR . 'modules';
+        array_unshift($hook_data, $plugin_modules);
+        return $hook_data;
+    }
+
+    public function welcomePowerOutages(array $hook_data = array())
+    {
+        $SMARTY = LMSSmarty::getInstance();
+
+        $gaid = ConfigHelper::getConfig('tauron.gaid');
+        $type = ConfigHelper::getConfig('tauron.type');
+        $api_url = ConfigHelper::getConfig('tauron.api_url');
+
+        if (!$gaid) {
+            $gaid = 502;
+        }
+
+        if (!$type) {
+            $type = 'commune';
+        }
+
+        if (!$api_url) {
+            $api_url = 'https://www.tauron-dystrybucja.pl/iapi/';
+        }
+
+        $full_url = $api_url . '/outage/GetOutages?gaid=' . $gaid . '&type=' . $type;
+
+        $CURLConnection = curl_init();
+        curl_setopt($CURLConnection, CURLOPT_URL, $full_url);
+        curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
+        $json = curl_exec($CURLConnection);
+        curl_close($CURLConnection);
+
+        $SMARTY->assign('powerOutages', json_decode($json, true));
+        return $hook_data;
+    }
+}
---
+++ lib/locale/pl/strings.php
@@ -0,0 +1,9 @@
+<?php
+
+$_LANG['Power outages'] = 'Wyłączenia energi';
+$_LANG['Disconnection'] = 'Odłączenie';
+$_LANG['planned'] = 'planowane';
+$_LANG['emergency'] = 'awaryjne';
+$_LANG['Today\'s power outages'] = 'Dzisiejsze przerwy w dostawie prądu';
+$_LANG['Planned power outages with 5 days in advance'] = 'Planowane przerwy w dostawie prądu z 5 dniowym wyprzedzeniem';
+$_LANG['No submissions.']='Brak zgłoszeń.';
