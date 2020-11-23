6105834c09496f806a51cae9c122e625a24000e4
Łukasz Kopiszka <lukasz@alfa-system.pl>
2020-11-23 14:53:07 +0100

    minor refactoring and added CURL error handling

--- modules/tauronpoweroutages.php
+++ modules/tauronpoweroutages.php
@@ -5,27 +5,35 @@ function powerOutages($url)
     $commune = explode(",", ConfigHelper::getConfig('tauron.commune'));
     $district = explode(",", ConfigHelper::getConfig('tauron.district'));
     $api_url = ConfigHelper::getConfig('tauron.api_url', 'https://www.tauron-dystrybucja.pl/iapi');
-    $array = array();
+    $result = array();
 
-    $CURLConnection = curl_init();
+    $ch = curl_init(); // CURL handle
 
     if (empty($district)) {
         foreach ($commune as $gaid) {
-            curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=commune");
-            curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
-            $json = curl_exec($CURLConnection);
-            $array = array_merge_recursive($array, json_decode($json, true));
+            curl_setopt($ch, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=commune");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            if (curl_exec($ch) === false) {
+                echo 'Curl error: ' . curl_error($ch);
+            } else {
+                $json = curl_exec($ch);
+                $result = array_merge_recursive($result, json_decode($json, true));
+            }
         }
     } else {
         foreach ($district as $gaid) {
-            curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=district");
-            curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
-            $json = curl_exec($CURLConnection);
-            $array = array_merge_recursive($array, json_decode($json, true));
+            curl_setopt($ch, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=district");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            if (curl_exec($ch) === false) {
+                echo 'Curl error: ' . curl_error($ch);
+            } else {
+                $json = curl_exec($ch);
+                $result = array_merge_recursive($result, json_decode($json, true));
+            }
         }
     }
-    curl_close($CURLConnection);
-    return $array;
+    curl_close($ch);
+    return $result;
 }
 
 $SMARTY->assign('power_outages', powerOutages($api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=" . $type));
