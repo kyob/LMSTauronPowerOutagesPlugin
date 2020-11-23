8b6f05179c0cba9b35cb9ba20943a2f9604dbc9d
Łukasz Kopiszka <lukasz@alfa-system.pl>
2020-11-23 14:38:53 +0100

    small refactoring

--- modules/tauronpoweroutages.php
+++ modules/tauronpoweroutages.php
@@ -2,31 +2,28 @@
 
 function powerOutages($url)
 {
-    $commune = ConfigHelper::getConfig('tauron.commune');
-    $commune = explode(",", $commune);
-
-    $district = ConfigHelper::getConfig('tauron.district');
-    $district = explode(",", $district);
-
+    $commune = explode(",", ConfigHelper::getConfig('tauron.commune'));
+    $district = explode(",", ConfigHelper::getConfig('tauron.district'));
     $api_url = ConfigHelper::getConfig('tauron.api_url', 'https://www.tauron-dystrybucja.pl/iapi');
     $array = array();
 
     $CURLConnection = curl_init();
 
-    foreach ($commune as $gaid) {
-        curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=commune");
-        curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
-        $json = curl_exec($CURLConnection);
-        $array = array_merge_recursive($array, json_decode($json, true));
+    if (empty($district)) {
+        foreach ($commune as $gaid) {
+            curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=commune");
+            curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
+            $json = curl_exec($CURLConnection);
+            $array = array_merge_recursive($array, json_decode($json, true));
+        }
+    } else {
+        foreach ($district as $gaid) {
+            curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=district");
+            curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
+            $json = curl_exec($CURLConnection);
+            $array = array_merge_recursive($array, json_decode($json, true));
+        }
     }
-
-    foreach ($district as $gaid) {
-        curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=district");
-        curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
-        $json = curl_exec($CURLConnection);
-        $array = array_merge_recursive($array, json_decode($json, true));
-    }
-
     curl_close($CURLConnection);
     return $array;
 }
