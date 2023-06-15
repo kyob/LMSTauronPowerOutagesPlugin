283f6e043a52394286869e9d92c89bea82b6a154
olimplan <lms@olimplan.pl>
2023-06-15 18:05:42 +0200

    Update TauronPowerOutagesHandler.php
    mod: welcomeTauronPowerOutages: added province support, updated API URL, updated API syntax

--- handlers/TauronPowerOutagesHandler.php
+++ handlers/TauronPowerOutagesHandler.php
@@ -35,31 +35,41 @@ class TauronPowerOutagesHandler
             $last_updated_cache = 0;
         }
 
-        $commune = ConfigHelper::getConfig('tauron.commune',502);
+        $commune = ConfigHelper::getConfig('tauron.commune');
         $commune = explode(",", $commune);
 
         $district = ConfigHelper::getConfig('tauron.district',6);
         $district = explode(",", $district);
 
-        $api_url = ConfigHelper::getConfig('tauron.api_url', 'https://www.tauron-dystrybucja.pl/iapi');
+        $province = ConfigHelper::getConfig('tauron.province',12);
+        $province = explode(",", $province);
+        
+        $api_url = ConfigHelper::getConfig('tauron.api_url', 'https://www.tauron-dystrybucja.pl/waapi');
         $outages = array();
 
         if ((time() - $last_updated_cache) > $time_in_cache) {
 
+            $queryDateTime = date("Y-m-d") . "T" . date("H:i") . ":00.000Z";
+            
             $CURLConnection = curl_init();
 
-            foreach ($commune as $gaid) {
-                curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=commune");
-                curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
-                $json = curl_exec($CURLConnection);
-                $outages = array_merge_recursive($outages, json_decode($json, true));
-            }
-
-            foreach ($district as $gaid) {
-                curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outage/GetOutages?gaid=" . $gaid . "&type=district");
-                curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
-                $json = curl_exec($CURLConnection);
-                $outages = array_merge_recursive($outages, json_decode($json, true));
+            foreach ($province as $provinceGAID) {
+                foreach ($district as $districtGAID) {
+                    if (!empty($commune)) {
+                        foreach ($commune as $communeGAID) {
+                            curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outages/area?provinceGAID=" . $provinceGAID . "&districtGAID=" . $districtGAID . "&fromDate=" . $queryDateTime . "&toDate=" . $queryDateTime . "&communeGAID=" . $communeGAID);
+                            curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
+                            $json = curl_exec($CURLConnection);
+                            $outages = array_merge_recursive($outages, json_decode($json, true));
+                        }
+                    }
+                    else {
+                            curl_setopt($CURLConnection, CURLOPT_URL, $api_url . "/outages/area?provinceGAID=" . $provinceGAID . "&districtGAID=" . $districtGAID . "&fromDate=" . $queryDateTime . "&toDate=" . $queryDateTime);
+                            curl_setopt($CURLConnection, CURLOPT_RETURNTRANSFER, true);
+                            $json = curl_exec($CURLConnection);
+                            $outages = array_merge_recursive($outages, json_decode($json, true));
+                    }
+                }
             }
 
             curl_close($CURLConnection);
